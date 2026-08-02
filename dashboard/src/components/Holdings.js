ca5f241b27a2a1a61a69d4142604ad1ff06c613e
Moksh Kumar <moksh06082005@gmail.com>
2026-08-02 20:27:37 +0530

    Added backend models and MongoDB integration

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios, { all } from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
-import { holdings } from "../data/data";
+// import { holdings } from "../data/data";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
-  // useEffect(() => {
-  //   axios.get("http://localhost:3002/allHoldings").then((res) => {
-  //     // console.log(res.data);
-  //     setAllHoldings(res.data);
-  //   });
-  // }, []);
   useEffect(() => {
-  setAllHoldings(holdings);
-}, []);
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
+      setAllHoldings(res.data);
+    });
+  }, []);
+//   useEffect(() => {
+//   setAllHoldings(holdings);
+// }, []);
 
   // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   const labels = allHoldings.map((subArray) => subArray["name"]);
